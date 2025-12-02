0a9e7dd23e9f17ab09e2adf5bf36661d123c0101
bgozlinski <bartlomiej.gozlinski@gmail.com>
2025-12-02 05:58:52 +0100

    fix(auth): changed TTL from 12h to 24h

--- services/auth/app/core/config.py
+++ services/auth/app/core/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
 
     JWT_SECRET_KEY: str
     JWT_ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     PROJECT_NAME: str = "Auth Service"
