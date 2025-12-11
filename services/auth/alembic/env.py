583281d05ab9196a71712c322ed08190f25a27e0
bgozlinski <bartlomiej.gozlinski@gmail.com>
2025-12-11 05:51:50 +0100

    update(auth): alembic env update

--- services/auth/alembic/env.py
+++ services/auth/alembic/env.py
@@ -4,7 +4,6 @@ from sqlalchemy import pool
 from alembic import context
 from app.core.config import settings
 from app.core.database import Base
-
 from app.models.user import User
 
 config = context.config
