eca75ebaaf5a96fe877ff59fa2aa957d26855e6a
bgozlinski <bartlomiej.gozlinski@gmail.com>
2025-12-17 00:38:30 +0100

    add filter Product.is_active == True for get_products, add product.is_active = False on delete_products

--- services/product-catalog/app/repositories/product.py
+++ services/product-catalog/app/repositories/product.py
@@ -3,6 +3,7 @@ from typing import Optional, List
 from app.models.product import Product
 from app.schemas.product import ProductCreate
 from fastapi import HTTPException, status
+from datetime import datetime
 
 def get_product_by_id(db: Session, product_id: int) -> Optional[Product]:
     return db.query(Product).filter(Product.id == product_id).first()
@@ -12,7 +13,13 @@ def get_products(
         skip: int = 0,
         limit: int = 100
 ) -> List[Product]:
-    return db.query(Product).offset(skip).limit(limit).all()
+    return (
+        db.query(Product)
+        .filter(Product.is_active == True)
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
 
 def create_product(db: Session, product: ProductCreate) -> Product:
     db_product = Product(**product.model_dump())
@@ -25,6 +32,8 @@ def delete_product(db: Session, product_id: int) -> bool:
     product = get_product_by_id(db, product_id)
     if not product:
         return False
-    db.delete(product)
-    db.commit()
+    if product.is_active:
+        product.is_active = False
+        product.updated_at = datetime.utcnow()
+        db.commit()
     return True
