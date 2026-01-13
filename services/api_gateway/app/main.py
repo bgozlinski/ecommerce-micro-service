2b04e4e7367c9606d3b7f5f590c3c23915ddc534
bgozlinski <bartlomiej.gozlinski@gmail.com>
2026-01-13 17:28:04 +0100

    feat(api-gateway): add order and inventory service routes

--- services/api_gateway/app/main.py
+++ services/api_gateway/app/main.py
@@ -11,12 +11,16 @@ from app.core.route_wrapper import route
 from app.routes.auth import router as auth_router
 from app.routes.users import router as users_router
 from app.routes.product import router as product_router
+from app.routes.order import router as order_router
+from app.routes.inventory import router as inventory_router
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
 app.include_router(auth_router, prefix=settings.API_V1_PREFIX, tags=["auth"])
 app.include_router(users_router, prefix=settings.API_V1_PREFIX, tags=["users"])
 app.include_router(product_router, prefix=settings.API_V1_PREFIX, tags=["product-catalog"])
+app.include_router(inventory_router, prefix=settings.API_V1_PREFIX, tags=["inventory"])
+app.include_router(order_router, prefix=settings.API_V1_PREFIX, tags=["orders"])
 
 @app.get("/health")
 async def health_check():
